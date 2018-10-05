e7338f03842eb647bc9e7c01339ef4d313d1f4af
ashna111 <ashnashah111@gmail.com>
2018-10-05 16:52:28 +0530

    add models

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+//Models
+const User=require('./models/user');
+const Event=require('./models/event');
+const Lecture=require('./models/lecture');
+const Reminder=require('./models/reminder');
+const Subject=require('./models/subject');
+const Submission=require('./models/submission');
+
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
 const port = process.env.PORT || 3000;
+const db = process.env.DATABASEURL || 'mongodb://localhost/scheduletracker';
+
+mongoose.connect(db);
+
 
 app.listen(port, () => {
   console.log('Server started on ' + port);
