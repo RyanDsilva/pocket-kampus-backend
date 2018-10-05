bd844439ccc39a4fc828f22adbd4dd3a160c20bd
Ryan <ryan.dsilva.98@gmail.com>
2018-10-05 14:00:34 +0530

    Init

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Server started on ' + port);
+});
