556a05a103947bdfbe5713f1b0cbe7a3bee1a578
Nehaa-Patil <nehappatil232@gmail.com>
2018-10-05 19:32:04 +0530

    subject routes update

--- routes/subjects.js
+++ routes/subjects.js
@@ -1,77 +1,118 @@
 var express = require('express');
 var router = express.Router();
-var Reminder = require('../models/subject');
+var Subject = require('../models/subject');
+var User = require('../models/user');
 
 //show all
-router.get('/subjects', function(req, res){
-  Subject.find({}, function(err, subjects){
+router.get('/users/:id/subjects', function(req, res){
+  User.findById(req.params.id, function(err, user){
     if(err){
       res.status(500).json(err);
     } else {
-      res.status(200).json(subjects);
+      Subject.find({}, function(err, subjects){
+        if(err){
+          res.status(500).json(err);
+        } else {
+          res.status(200).json(subjects);
+        }
+      })
     }
   })
 })
 //create new subject
-router.post('/subjects', function(req, res) {
-  Subject.create({ title: req.body.subject.name }, function(err, subject) {
-    if (err) {
+router.post('/users/:id/subjects', function(req, res) {
+  User.findById(req.params.id, function(err, user){
+    if(err){
       res.status(500).json(err);
     } else {
-      subject.teacher = req.body.subject.teacher;
-      subject.resources = req.body.subject.resources;
-      subject.save();
-      res.status(200).json(subject);
+      Subject.create({ title: req.body.subject.name }, function(err, subject) {
+        if (err) {
+          res.status(500).json(err);
+        } else {
+          subject.teacher = req.body.subject.teacher;
+          subject.resources = req.body.subject.resources;
+          subject.save();
+          user.subjects.push(subject);
+          user.save();
+          res.status(200).json(subject);
+        }
+      });
     }
-  });
+  })
+  
 });
 
 //show details of subject
-router.get('/subjects/:id', function(req, res){
-  Subject.findById(req.params.id, function(err, foundSubject) {
-    if (err) {
+router.get('users/:id/subjects/:subject_id', function(req, res){
+  User.findById(req.params.id, function(err, user){
+    if(err){
       res.status(500).json(err);
     } else {
-      res.status(200).json(foundSubject);
+      Subject.findById(req.params.subject_id, function(err, foundSubject) {
+        if (err) {
+          res.status(500).json(err);
+        } else {
+          res.status(200).json(foundSubject);
+        }
+      });
     }
-  });
+  })
 })
 
 //Edit
-router.get('/subjects/:id/edit', function(req, res) {
-  Subject.findById(req.params.id, function(err, foundSubject) {
-    if (err) {
+router.get('users/:id/subjects/:subject_id/edit', function(req, res) {
+  User.findById(req.params.id, function(err, user){
+    if(err){
       res.status(500).json(err);
     } else {
-      res.status(200).json(foundSubject);
+      Subject.findById(req.params.subject_id, function(err, foundSubject) {
+        if (err) {
+          res.status(500).json(err);
+        } else {
+          res.status(200).json(foundSubject);
+        }
+      });
     }
-  });
+  })
 });
 
 //Update
-router.put('/Subjects/:id/edit', function(req, res) {
-  Subject.findByIdAndUpdate(
-    req.params.id,
-    req.body.subject,
-    { new: true },
-    function(err, updatedSubject) {
-      if (err) {
-        res.status(500).json(err);
-      } else {
-        res.status(200).json(updatedSubject);
-      }
-    }
-  );
-});
-
-router.delete('/subjects/:id/delete', function(req, res) {
-  Subject.findByIdAndRemove(req.params.id, function(err) {
-    if (err) {
+router.put('users/:id/subjects/:subject_id/edit', function(req, res) {
+  User.findById(req.params.id, function(err, user){
+    if(err){
       res.status(500).json(err);
     } else {
-      res.status(200).json('Deleted Subject');
+      Subject.findByIdAndUpdate(
+        req.params.subject_id,
+        req.body.subject,
+        { new: true },
+        function(err, updatedSubject) {
+          if (err) {
+            res.status(500).json(err);
+          } else {
+            res.status(200).json(updatedSubject);
+          }
+        });
     }
-  });
+  })
+  
+});
+
+router.delete('users/:id/subjects/:subject_id/delete', function(req, res) {
+  User.findById(req.params.id, function(err, user){
+    if(err){
+      res.status(500).json(err);
+    } else {
+      Subject.findByIdAndRemove(req.params.subject_id, function(err) {
+        if (err) {
+          res.status(500).json(err);
+        } else {
+          res.status(200).json('Deleted Subject');
+        }
+      });
+    }
+  })
+  
 });
 
 module.exports = router;
